9244423f787043faf3aea832876e6a707b9bdea6
Danjuma1 <hda0202@gmail.com>
2023-03-14 12:32:36 +0100

    created views for home and dashboard pages

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_view(request):
+    return render(request, 'core/home.html')
+
+def dashboard_view(request):
+    return render(request, 'core/dashboard.html')
