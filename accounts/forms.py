d0a2bd1272461759edb46629d6033781c7add263
Danjuma1 <hda0202@gmail.com>
2023-03-14 12:38:01 +0100

    added age range field to usercreation form and crerated its choices

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,26 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
+AGE_RANGE_CHOICES = (
+	('Under 12 years old', 'Under 12 years old'),
+	('12-17 years old', '12-17 years old'),
+	('18-24 years old', '18-24 years old'),
+	('25-34 years old', '25-34 years old'),
+	('35-44 years old', '35-44 years old'),
+	('45-54 years old', '45-54 years old'),
+	('55-64 years old', '55-64 years old'),
+	('65-74 years old', '65-74 years old'),
+	('Above 74 years old', 'Above years old'),
+)
+
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
+	age_range = forms.ChoiceField(choices=AGE_RANGE_CHOICES, required=False)
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username", "email", "age_range", "password1", "password2")
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
