5507444c32f253ca0318c59a4cdf5204c61314f6
Danjuma1 <hda0202@gmail.com>
2023-03-14 12:36:29 +0100

    created user profile model add added age range as variable

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age_range = models.CharField(max_length=50, default="Choose One")
+    
+    def __str__(self):
+        return self.user.username + " - " + self.age_range
