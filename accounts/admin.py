88a836e90bee0a0999dd1ee4187a524e4bf3e42a
Danjuma1 <hda0202@gmail.com>
2023-03-14 12:41:07 +0100

    registered userprofile model in accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile, User
+
+class UserProfileAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    # list_display = ('username', 'email', 'age_range')
+
+admin.site.register(UserProfile)
