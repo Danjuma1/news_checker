196a15f0ed9199f5a8146b88e5ae89e782d226d1
Danjuma1 <hda0202@gmail.com>
2023-03-20 13:49:02 +0100

    added __str__ function to news model

--- core/models.py
+++ core/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class News(models.Model):
     headline = models.CharField(max_length=150)
     content = models.TextField()
+
+    def __str__(self):
+        return self.headline
