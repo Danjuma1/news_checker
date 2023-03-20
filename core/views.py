a1da7104dcc7c0fafec017d8f3ae5fc54e0ca69a
Danjuma1 <hda0202@gmail.com>
2023-03-20 13:51:16 +0100

    edited dashboard views to handle news form and check news list

--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import News
+from .forms import NewsForm
 
 
 def home_view(request):
     return render(request, 'core/home.html')
 
 def dashboard_view(request):
-    return render(request, 'core/dashboard.html')
+    checked_news = News.objects.all().order_by("-id")
+    form = NewsForm
+    if request.method == "POST":
+        form = NewsForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("core:dashboard")
+    context={"form": form, "checked_news": checked_news}
+    return render(request, 'core/dashboard.html', context)
