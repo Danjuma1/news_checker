d1ffeb094ad10f3a5dd4329e1daf37ba8dfc245b
Danjuma1 <hda0202@gmail.com>
2023-03-27 22:33:32 +0100

    woriking on the ml model prediction, passing the filed data to the model but having issues unresolved bcos there's no vectorizer.sav

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from .models import News
 from .forms import NewsForm
+
+pac = joblib.load('modeling.sav')
 
 
 def home_view(request):
@@ -10,12 +17,15 @@ def home_view(request):
 
 @login_required
 def dashboard_view(request):
+    if request.method == 'GET':
+        form = NewsForm()
     checked_news = News.objects.all().order_by("-id")
-    form = NewsForm
-    if request.method == "POST":
-        form = NewsForm(request.POST)
-        if form.is_valid:
-            form.save()
-            return redirect("core:dashboard")
     context={"form": form, "checked_news": checked_news}
     return render(request, 'core/dashboard.html', context)
+
+@login_required
+def result_view(request):
+    form_param = request.GET
+    news_content = form_param['content']
+
+    ans = pac.predict(news_content)
