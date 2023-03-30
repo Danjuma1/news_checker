6205594b7a6dacd08ae4b69c9d17a701d235e888
Danjuma1 <hda0202@gmail.com>
2023-03-31 00:32:50 +0100

    created new urls

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,6 @@ app_name = "core"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
+    path("result/", views.result_view, name="result"),
+    # path("save/", views.save_view, name="save")
 ]
