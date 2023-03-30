b919d585ae846df4f0c1751b5b2f88e60abe656f
Danjuma1 <hda0202@gmail.com>
2023-03-31 00:31:52 +0100

    created NewsAdmin and registered it to manage News in admin interface

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import News
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'content', 'author', 'label')
 
-admin.site.register(News)
+admin.site.register(News, NewsAdmin)
