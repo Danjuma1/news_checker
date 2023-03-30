9cd5f8cd80d0d0760c722927c0f11791437f7467
Danjuma1 <hda0202@gmail.com>
2023-03-31 00:30:02 +0100

    edited the News model to add default values fpr author and created the label field@

--- core/models.py
+++ core/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class News(models.Model):
-    headline = models.CharField(max_length=150)
-    author = models.CharField(max_length=10)
+    headline = models.CharField(max_length=200)
+    author = models.CharField(max_length=100, default="Unkown", blank=True)
     content = models.TextField()
+    label = models.CharField(max_length=10, default="Fake News")
 
     def __str__(self):
         return self.headline
