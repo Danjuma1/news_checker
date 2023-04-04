82ea188872630c7c81064cb39bd9a277dde10858
Danjuma1 <hda0202@gmail.com>
2023-04-04 11:54:30 +0100

    removed default value from label in News model

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class News(models.Model):
     headline = models.CharField(max_length=200)
     author = models.CharField(max_length=100, default="Unkown", blank=True)
     content = models.TextField()
-    label = models.CharField(max_length=10, default="Fake News")
+    label = models.CharField(max_length=10)
 
     def __str__(self):
         return self.headline
