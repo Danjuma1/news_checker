5783a8284c1d3b4668bf046945a36cd28ba8fa84
Danjuma1 <hda0202@gmail.com>
2023-04-04 11:50:45 +0100

    added urls for 3 new pages

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ app_name = "core"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
-    path("result/", views.result_view, name="result"),
-    # path("save/", views.save_view, name="save")
+    # path("result/", views.result_view, name="result"),
+    path("all_news/", views.news_list_view, name="news_list"),
+    path("all_true_news/", views.true_news_list_view, name="true_news_list"),
+    path("all_fake_news/", views.fake_news_list_view, name="fake_news_list"),
 ]
