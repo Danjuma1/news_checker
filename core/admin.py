0f2ef56864566347d29b95bb77724ec547eedcd5
Danjuma1 <hda0202@gmail.com>
2023-04-07 13:29:55 +0100

    added user and date_added columns to News table in admin

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import News
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('headline', 'content', 'author', 'label')
+    list_display = ('user', 'headline', 'content', 'author', 'label', 'date_added')
 
 admin.site.register(News, NewsAdmin)
