035b098fd0eedbc9ba6a20bbf01cd09eb4244d4b
Danjuma1 <hda0202@gmail.com>
2023-04-07 13:28:58 +0100

    added user and date_added fields to News model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class News(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     headline = models.CharField(max_length=200)
     author = models.CharField(max_length=100, default="Unkown", blank=True)
     content = models.TextField()
     label = models.CharField(max_length=10)
+    date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.headline
