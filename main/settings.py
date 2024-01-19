1d1a164bc7975756af81f5aa1a8ba682db48b584
Danjuma1 <hda0202@gmail.com>
2024-01-19 10:11:22 +0100

    recreated the repository

--- main/settings.py
+++ main/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
-        'NAME'    : 'fake_news_dectection',              # <-- UPDATED line 
-        'USER'    : '',                     # <-- UPDATED line
-        'PASSWORD': '',              # <-- UPDATED line
-        'HOST'    : 'localhost',                # <-- UPDATED line
-        'PORT'    : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
+#         'NAME'    : 'fake_news_dectection',              # <-- UPDATED line 
+#         'USER'    : '',                     # <-- UPDATED line
+#         'PASSWORD': '',              # <-- UPDATED line
+#         'HOST'    : 'localhost',                # <-- UPDATED line
+#         'PORT'    : '3306',
+#     }
+# }
 
 
 # Password validation
